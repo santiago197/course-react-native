f957a158ed101025f8f99169ceefaa57542dbbf3
santiago197 <santinacional123.sr@gmail.com>
2024-02-28 17:14:13 -0500

    Envio y valores de form

--- 01-react-foundation/src/components/FormsPage.tsx
+++ 01-react-foundation/src/components/FormsPage.tsx
@@ -6,29 +6,35 @@ type FormInputs = {
 };
 
 export const FormsPage = () => {
-	const { register } = useForm<FormInputs>({
+	const { register, handleSubmit, formState, watch } = useForm<FormInputs>({
 		defaultValues: {
 			email: 'santiago.rodriguez@gmail.com',
 			password: 'Abcc213',
 		},
 	});
-
+	const onSubmit = (myForm: FormInputs) => {
+		console.log({ myForm });
+	};
+	console.log(watch('email'));
 	return (
 		<>
-			<h3>Formularios</h3>
-			<div style={{ display: 'flex', flexDirection: 'column' }}>
-				<input
-					type="text"
-					placeholder="Email"
-					{...register('email')}
-				/>
-				<input
-					type="text"
-					placeholder="Password"
-					{...register('password')}
-				/>
-				<button type="submit">Ingresar</button>
-			</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<h3>Formularios</h3>
+				<div style={{ display: 'flex', flexDirection: 'column' }}>
+					<input
+						type="text"
+						placeholder="Email"
+						{...register('email', { required: true })}
+					/>
+					<input
+						type="text"
+						placeholder="Password"
+						{...register('password')}
+					/>
+					<button type="submit">Ingresar</button>
+				</div>
+			</form>
+			<pre>{JSON.stringify(formState, null, 2)}</pre>
 		</>
 	);
 };
