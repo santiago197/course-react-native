6a6d885c38c584c7fdadfbeafbd02febe1637da8
santiago197 <santinacional123.sr@gmail.com>
2024-02-28 16:06:03 -0500

    Login y Logout con Zustand

--- 01-react-foundation/src/components/LoginPage.tsx
+++ 01-react-foundation/src/components/LoginPage.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useAuthStore } from '../store/auth.store';
 
 export const LoginPage = () => {
-	const authState = useAuthStore((state) => state.status);
+	const authStatus = useAuthStore((state) => state.status);
+	const login = useAuthStore((state) => state.login);
+	const logout = useAuthStore((state) => state.logout);
+	const user = useAuthStore((state) => state.user);
+	useEffect(() => {
+		setTimeout(() => {
+			logout();
+		}, 1500);
+	}, []);
+
+	if (authStatus == 'checking') {
+		return <h3>Loading...</h3>;
+	}
 	return (
 		<>
 			<h3>Login Page</h3>
-			<span>{authState}</span>
+			{authStatus === 'authenticated' ? (
+				<div>Autenticado como {JSON.stringify(user, null, 2)}</div>
+			) : (
+				<div>No autenticado</div>
+			)}
+
+			{authStatus == 'authenticated' ? (
+				<button onClick={logout}>Logout</button>
+			) : (
+				<button onClick={() => login('santiago.rodriguez@outlook.com', 'ABC')}>
+					Login
+				</button>
+			)}
 		</>
 	);
 };
